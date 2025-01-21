d8d9e9fee9ff039009ae9fbb3f2159b95d127fad
Rolando Serrano <roserranolevican@gmail.com>
2025-01-21 16:20:47 -0300

    Data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTraininigPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 logger.info('Welcome to the custom logging data science')
 
@@ -20,7 +21,6 @@ except Exception as e:
 
 STAGE_NAME = 'Model Evaluation stage'
 
-
 try:
     logger.info(f'>>>> stage {STAGE_NAME} started <<<<<')
     data_ingestion = DataValidationTraininigPipeline()
@@ -29,3 +29,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = 'Data Transformation stage'
+
+try:
+    logger.info(f'>>>> stage {STAGE_NAME} started <<<<<')
+    data_ingestion = DataTransformationTrainingPipeline()
+    data_ingestion.initiate_data_transformation()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed\n\nx=============x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
